10c6fd7bfa2dc7db8fe76176a2035a08ce1ed08a
MIshmail <michaely.ishmail@gmail.com>
2022-04-22 14:58:22 -0400

    added loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,13 +8,11 @@ console.log(playerName, playerHealth, playerAttack);
 // By putting the variable element before the window prompt. we are able to capture and store the players input in a variable
 // that can be used later in the code.
 
-var enemyName = 'Roborto';
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = '50';
 var enemyAttack = '12';
 
-
-
-var fight = function(){
+var fight = function(enemyName){
 
     window.alert ('Welcome to Robot Gladiators!');
 
@@ -81,4 +79,7 @@ var fight = function(){
 // In javascript, you must first create the function and then call it. The function will not run if it is not called. Above is
 // an example of a stored function. Below is an example of a called function.
 
-fight ();
+for (i = 0; i < enemyNames.length; i++){
+    fight(enemyNames[i]);
+}
+
